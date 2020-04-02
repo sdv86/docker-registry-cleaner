e8037c736e255f33b3853733711b193fb2d8bf80
s.gerasimov <s.gerasimov@fbs.com>
2020-04-02 17:25:53 +0300

    fixed & refactored; now we correctly close connects

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # Docker registry v2 cleaner application
+
 ### How to use it:
-* Clone the repo
-* Don`t forget to prepare & edit the config:
+
+- Clone the repo
+- Don`t forget to prepare & edit the config:
+
 ```bash
 # mv config.example.toml config.toml && vim config.toml
 ```
-* Build the application
+
+- Build the application
+
 ```bash
 # go build
 ```
-* Run builded binary to clean the registry
+
+- Run builded binary to clean the registry
+
 ```bash
 # ./docker-registry-cleaner
 ```
-* Profit!
+
+- Profit!
--- functions.go
+++ functions.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -10,64 +11,86 @@ import (
 )
 
 const (
-	drCatalogUrl = "/v2/_catalog?n=1000"
-	drTagsList = "/tags/list?n=1000"
+	drCatalogURL = "/v2/_catalog?n=1000"
+	drTagsList   = "/tags/list?n=1000"
 )
 
+// Tag ...
 type Tag string
+
+// Repo ...
 type Repo string
+
+// Tags ...
 type Tags struct {
 	Name string
 	Tags []Tag `json:"tags"`
 }
+
+// Repos ...
 type Repos struct {
 	Repositories []Repo `json:"repositories"`
 }
+
+// ImageDigest ...
 type ImageDigest struct {
 	Digest map[string]string
 }
+
+// Images ...
 type Images struct {
-	Name string
+	Name    string
 	Digests ImageDigest
 }
+
+// V1Manifest ...
 type V1Manifest struct {
-	Name string `json:"name"`
-	Tag string `json:"tag"`
+	Name    string    `json:"name"`
+	Tag     string    `json:"tag"`
 	History []History `json:"history"`
 }
+
+// History ...
 type History struct {
 	V1Compatibility string `json:"v1Compatibility"`
 }
 
-
 func makeV2Request(url, m string, c *Config) *http.Response {
-	req, err := http.NewRequest(m, url, nil)
-	if err != nil {
-		log.Println(err)
+	config := &tls.Config{
+		InsecureSkipVerify: true,
 	}
+	tr := &http.Transport{TLSClientConfig: config}
+	client := &http.Client{Transport: tr}
+
+	req, _ := http.NewRequest(m, url, nil)
 	req.SetBasicAuth(c.DrUser, c.DrPass)
 	req.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v2+json")
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 	}
+	defer client.CloseIdleConnections()
 	return resp
 }
 
 func makeV1Request(url, m string, c *Config) *http.Response {
-	req, err := http.NewRequest(m, url, nil)
-	if err != nil {
-		log.Println(err)
+	config := &tls.Config{
+		InsecureSkipVerify: true,
 	}
+	tr := &http.Transport{TLSClientConfig: config}
+	client := &http.Client{Transport: tr}
+	req, _ := http.NewRequest(m, url, nil)
 	req.SetBasicAuth(c.DrUser, c.DrPass)
 	req.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v1+json")
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 	}
+	defer client.CloseIdleConnections()
 	return resp
 }
 
+// DelManifest ...
 func DelManifest(c *Config, registry, manifest string) string {
 	resp := makeV2Request(c.DrHost+":"+c.DrPort+"/v2/"+registry+"/manifests/"+manifest, "DELETE", c)
 	_, err := ioutil.ReadAll(resp.Body)
@@ -77,11 +100,21 @@ func DelManifest(c *Config, registry, manifest string) string {
 	return resp.Status
 }
 
-func timeToUnix(ts string) string  {
+func timeToUnix(ts string) string {
 	t, _ := time.Parse(time.RFC3339, ts)
-	return strconv.FormatInt(t.Unix(),10)
+	return strconv.FormatInt(t.Unix(), 10)
 }
 
+func unixToTime(date string) *time.Time {
+	i, err := strconv.ParseInt(date, 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	tm := time.Unix(i, 0)
+	return &tm
+}
+
+// GetCreationDate ...
 func GetCreationDate(c *Config, registry, tagName string) string {
 
 	resp := makeV1Request(c.DrHost+":"+c.DrPort+"/v2/"+registry+"/manifests/"+tagName, "GET", c)
@@ -97,10 +130,12 @@ func GetCreationDate(c *Config, registry, tagName string) string {
 	in := []byte(v1manifest.History[0].V1Compatibility)
 	var raw map[string]interface{}
 	json.Unmarshal(in, &raw)
+	// fmt.Println(" ", raw["created"])
 	ct := timeToUnix(raw["created"].(string))
 	return ct
 }
 
+// GetManifest ...
 func GetManifest(c *Config, registry, tagName string) (string, string) {
 	resp := makeV2Request(c.DrHost+":"+c.DrPort+"/v2/"+registry+"/manifests/"+tagName, "GET", c)
 	_, err := ioutil.ReadAll(resp.Body)
@@ -110,6 +145,7 @@ func GetManifest(c *Config, registry, tagName string) (string, string) {
 	return resp.Header.Get("Docker-Content-Digest"), resp.Status
 }
 
+// ListTags ...
 func ListTags(c *Config, registry string) *Tags {
 	resp := makeV2Request(c.DrHost+":"+c.DrPort+"/v2/"+registry+drTagsList, "GET", c)
 	body, err := ioutil.ReadAll(resp.Body)
@@ -124,8 +160,9 @@ func ListTags(c *Config, registry string) *Tags {
 	return &tags
 }
 
+// ListImages ...
 func ListImages(c *Config) *Repos {
-	resp := makeV2Request(c.DrHost+":"+c.DrPort+drCatalogUrl, "GET", c)
+	resp := makeV2Request(c.DrHost+":"+c.DrPort+drCatalogURL, "GET", c)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
